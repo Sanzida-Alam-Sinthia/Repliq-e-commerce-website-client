1e5d0f32a64af17eceb05591b7ecd7587ed8525a
Sanzida-Alam-Sinthia <sanzidaalamsinthia@gmail.com>
2023-01-25 07:07:31 +0600

    first commit

---
+++ src/Pages/Home/Banner/Banner.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import banner from '../../../Assests/Banner.JPG'
+const Banner = () => {
+    return (
+        <div className=' mt-5 '>
+
+            <div class="flex flex-col w-1/2 gap-5 p-3 mx-auto  select-none sm:p-4 sm:h-64 rounded-2xl sm:flex-row ">
+                <div class=" sm:h-full sm:w-72 rounded-xl ">
+                    <img src={banner} alt="" />
+                </div>
+                <div class="flex ">
+                    <div class="flex flex-col flex-3 gap-4">
+                        <div class="w-full font-bold text-3xl  ">
+                            <h2> Welcome To Repliq E-Commerce Site!</h2>
+                        </div>
+                        <div class="w-full font-semibold  ">
+                            <h2>Trustable ad Reliable Market place with great user experience</h2>
+                        </div>
+
+
+                    </div>
+
+                </div>
+            </div>
+
+        </div>
+    );
+};
+
+export default Banner;
---
+++ src/Pages/Shared/Navbar/Navbar.js
@@ -0,0 +1,78 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <div>
+
+            <div>
+                <nav class="bg-indigo-200 dark:bg-gray-800  shadow ">
+                    <div class="px-8 mx-auto max-w-7xl">
+                        <div class="flex items-center justify-between h-16">
+                            <div class=" flex items-center">
+                                <a class="flex-shrink-0 font-bold" href="/">
+                                    REPLIQ
+                                </a>
+                                <div class="hidden md:block">
+                                    <div class="flex items-baseline ml-10 space-x-4">
+                                        <a class="text-black hover:text-indigo-700 dark:hover:text-white px-3 py-2 rounded-md text-md font-medium" href="/">
+                                            Home
+                                        </a>
+
+                                        <a class="text-black hover:text-indigo-700 dark:hover:text-white px-3 py-2 rounded-md text-md font-medium" href="/#">
+                                            Admin Dashboard
+                                        </a>
+                                        <a class="text-black hover:text-indigo-700 dark:hover:text-white px-3 py-2 rounded-md text-md font-medium" href="/#">
+                                            Contact
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="block">
+                                <div class="flex items-center ml-4 md:ml-6">
+                                    <a class="p-1 text-black hover:text-indigo-700 rounded-full focus:outline-none  focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 font-bold focus:ring-white" href='/login'>
+                                        Login
+                                    </a>
+                                    <div class="relative ml-3">
+                                        <div class="relative inline-block text-left">
+                                            <div>
+                                                <button type="button" class="  flex items-center justify-center w-full rounded-md  px-4 py-2 text-sm font-bold text-black hover:text-indigo-700 dark:text-gray-50 hover:bg-gray-50 dark:hover:bg-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-gray-500" id="options-menu">
+                                                    View Cart
+                                                </button>
+                                            </div>
+
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="flex -mr-2 md:hidden">
+                                {/* <button class="text-gray-800 dark:text-white hover:text-gray-300 inline-flex items-center justify-center p-2 rounded-md focus:outline-none">
+                                    <svg width="20" height="20" fill="currentColor" class="w-8 h-8" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
+                                        <path d="M1664 1344v128q0 26-19 45t-45 19h-1408q-26 0-45-19t-19-45v-128q0-26 19-45t45-19h1408q26 0 45 19t19 45zm0-512v128q0 26-19 45t-45 19h-1408q-26 0-45-19t-19-45v-128q0-26 19-45t45-19h1408q26 0 45 19t19 45zm0-512v128q0 26-19 45t-45 19h-1408q-26 0-45-19t-19-45v-128q0-26 19-45t45-19h1408q26 0 45 19t19 45z">
+                                        </path>
+                                    </svg>
+                                </button> */}
+                            </div>
+                        </div>
+                    </div>
+                    <div class="md:hidden">
+                        <div class="px-2 pt-2 pb-3 space-y-1 sm:px-3">
+                            <a class="text-black hover:text-indigo-700 dark:hover:text-white block px-3 py-2 rounded-md text-base font-medium" href="/#">
+                                Home
+                            </a>
+
+                            <a class="text-black hover:text-indigo-700 dark:hover:text-white block px-3 py-2 rounded-md text-base font-medium" href="/#">
+                                Content
+                            </a>
+                            <a class="text-black hover:text-indigo-700 dark:hover:text-white block px-3 py-2 rounded-md text-base font-medium" href="/#">
+                                Contact
+                            </a>
+                        </div>
+                    </div>
+                </nav>
+            </div>
+
+        </div>
+    );
+};
+
+export default Navbar;
