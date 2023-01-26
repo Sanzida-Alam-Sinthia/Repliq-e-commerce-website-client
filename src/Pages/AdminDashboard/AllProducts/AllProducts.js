fe7e5e3dcb0cc2707d5ad72e315e754ddf56efec
Sanzida-Alam-Sinthia <sanzidaalamsinthia@gmail.com>
2023-01-26 13:50:23 +0600

    webtoken implemented

--- src/Pages/AdminDashboard/AllProducts/AllProducts.js
+++ src/Pages/AdminDashboard/AllProducts/AllProducts.js
@@ -1,6 +1,16 @@
+import { useQuery } from '@tanstack/react-query';
 import React from 'react';
 
 const AllProducts = () => {
+    const { data: users = [], refetch } = useQuery({
+        queryKey: ['users'],
+        queryFn: async () => {
+            const res = await fetch('http://localhost:5000/users');
+            const data = await res.json();
+            return data;
+        }
+    });
+
     return (
         <div>
             <h2>this is all products</h2>
